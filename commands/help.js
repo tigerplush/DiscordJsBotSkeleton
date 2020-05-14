6674bde046e34f30497917e04fe4bd6dc3c423bb
Tig0r <christopher.putz@sheep-records.de>
2020-05-14 23:44:08 +0200

    switched to string literals and removed trailing whitespaces

--- commands/help.js
+++ commands/help.js
@@ -13,38 +13,37 @@ module.exports =
     execute(message, args)
     {
         const {commands} = message.client;
-        
-        let answer = "";
-        answer += "`" + prefix;
+
+        let answer = `\`${prefix} `;
         if(!args.length)
         {
-            answer += this.name + "`\n" + this.description + "\n";
-            answer += "Available commands: ";
-            commands.map(command => {
+            answer += `${this.name}\`\n`
+            answer += `${this.description}\n`;
+            answer += `Available commands: \`${prefix} `;
+            answer += commands.map(command => {
                 if(!command.hidden)
                 {
-                    answer += "`" + prefix + command.name + "`, ";
+                    return command.name;
                 }
-            });
-            answer = answer.slice(0, answer.length - 2);
-            answer += "\n";
-            answer += "Get more info with `" + prefix + commands.get("help").name +" command`";
+            }).join(`\`, \`${prefix} `);
+            answer += `\`\nGet more info with \`${prefix} ${commands.get("help").name} command\``;
         }
         else
         {
             if(commands.has(args[0]))
             {
-                command = commands.get(args[0])
-                answer += command.name + " " + command.usage + "`\n" + command.description + "\n";
-                answer += "Examples:"
+                command = commands.get(args[0]);
+                answer += `${[command.name, command.usage].join(` `).trim()}\`\n`
+                answer += `${command.description}\n`;
+                answer += `Examples:`
                 command.example.forEach(example =>
                     {
-                        answer += "\n`" + prefix + command.name + " " + example.join("` => ");
+                        answer += `\n\`${prefix} ${[command.name, example[0]].join(` `).trim()}\`: ${example[1]}`;
                     });
             }
             else
             {
-                answer += args[0] + "` is not a valid command";
+                answer += `${args[0]}\` is not a valid command`;
             }
         }
         message.channel.send(answer);
